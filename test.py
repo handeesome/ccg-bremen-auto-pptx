dcd8e96081b9a0d6ddbab44fcec72ba01388c6f9
Dee Du <ducenhandee@gmail.com>
2025-02-07 07:48:17 +0800

    created card for form-section

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
-from functions.helper import *
-from pptx import Presentation
-from functions.奉献回应礼 import partEleven
+import pythoncom
+import win32com.client
 
-prs = Presentation('docs/template.pptx')
-partEleven(prs, '林后 9:6-7')
-prs.save('test.pptx')
+# Initialize PowerPoint application
+pythoncom.CoInitialize()
+ppt_app = win32com.client.Dispatch("PowerPoint.Application")
+ppt_app.Visible = 1
+
+# Open the presentation
+ppt_path = r"C:\\Users\ducenhan\Desktop\\auto-pptx\\ccg-bremen-auto-pptx\\2024-10-06.pptx"
+presentation = ppt_app.Presentations.Open(ppt_path)
+
+# Define the output PDF path
+# pdf_path = r"C:\\Users\ducenhan\Desktop\\auto-pptx\\ccg-bremen-auto-pptx\\2024-10-06.pdf"
+
+# Save the presentation as PDF
+# presentation.SaveAs(pdf_path, 32)  # 32 is the constant for saving as PDF
+# presentation.Presentations.Add()
+# Close the presentation
+# presentation.Close()
+
+# Quit PowerPoint application
+# ppt_app.Quit()
+
+print("Presentation converted to PDF successfully.")
